7866cd41234a991f17a9100a620e7031f66ffe4c
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-02-23 12:21:23 +0545

    interface

--- NccLabSudip/Program.cs
+++ NccLabSudip/Program.cs
@@ -1,46 +1,23 @@
 ﻿using System;
-
-class rectangle
+abstract class player
 {
-    public int length;
-    public int breadth;
-
-    public rectangle(int length, int breadth)
-    {
-        this.length = length;
-        this.breadth = breadth;
-    }
-
-    public object this[int i]
-    {
-        get
-        {
-            if (i == 0)
-                return this.length;
-            else if (i == 1)
-                return this.breadth;
-            else
-                return null;
-        }
-        set
-        {
-            if (i == 0)
-                this.length = (int)value;
-            else if (i == 1)
-                this.breadth = (int)value;
-
-        }
-    }
+    public abstract void display();
+    
 }
 
+class coach : player
+{
+    public override void display()
+    {
+        Console.WriteLine("iam player and coach as well");
+    }
+}
 class program
 {
     static void Main(string[] args)
     {
-       rectangle rectangle1 = new rectangle(3,5);
-        rectangle1[0] = 9;
-        rectangle1[1] = 8;
-        Console.WriteLine("length is " + rectangle1[0] + " breadth is " + rectangle1[1]);
+        coach ram = new coach();
+        ram.display();
         Console.ReadLine(); 
     }
 }
