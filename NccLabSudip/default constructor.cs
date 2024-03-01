cacd3ed223c3b78aef70b3872b31d13529f0a84d
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-03-01 11:52:29 +0545

    default cons

---
+++ NccLabSudip/default constructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NccLabSudip
+{
+    internal class player
+    {
+        public player()
+        {
+            Console.WriteLine("this is the player");
+        }
+    }
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            player harry = new player();
+            Console.ReadLine();
+        }
+    }
+}
