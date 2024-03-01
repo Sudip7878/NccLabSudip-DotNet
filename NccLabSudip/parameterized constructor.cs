67eb02993e36a97d0743ef0ff69e48394da9b922
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-03-01 11:50:10 +0545

    parameter constructor

---
+++ NccLabSudip/parameterized constructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace NccLabSudip
+{
+    internal class player
+    {
+        public player(string name)
+        {
+            Console.WriteLine("hello " + name);
+        }
+    }
+    class test
+    {
+        static void Main(string[] args)
+        {
+            player harry = new player("harry");
+            Console.ReadLine();
+        }
+    }
+
+}
--- NccLabSudip/sustemio.cs
+++ NccLabSudip/sustemio.cs
@@ -2,22 +2,21 @@
 using System.IO;
 using System.Text;
 namespace NccLabSudip
+    //create file
 {
-    class program
+    class program1
     {
         static void Main(string[] args)
         {
             string filepath = @"C:\Users\SUDIP\Desktop\just\example.txt";
             using (FileStream fs = File.Create(filepath))
             {
-            byte[] data = new UTF8Encoding(true).GetBytes("hello world");
-            fs.Write(data, 0, data.Length);
-            Console.WriteLine("file created sucessfully");
-            Console.ReadLine();
+                byte[] data = new UTF8Encoding(true).GetBytes("hello world");
+                fs.Write(data, 0, data.Length);
+                Console.WriteLine("file created sucessfully");
+                Console.ReadLine();
 
             }
-           
-            
         }
     }
 }
