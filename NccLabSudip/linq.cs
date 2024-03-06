7d33dda5b03b3e73cf7e5982a76a9c121e2f197f
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-03-06 14:52:13 +0545

    update

---
+++ NccLabSudip/linq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace NccLabSudip
+{
+    class linq
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 5, 2, 7, 9, 1 };
+
+            var result = from data in arr where data > 5 select data;
+            Console.WriteLine("element greater than 5 are");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
